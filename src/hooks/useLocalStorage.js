bfea03d14a0e3dcf6a6d007e9ae4ff3463241241
Gregory Hawman <g.hawman@gmail.com>
2020-03-11 15:01:19 -0700

    finished the setter part of state in localStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -6,7 +6,10 @@ export default function useLocalStorage (key, initialValue) {
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
     });
-    return [storedValue];
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value))
+    };
 
-    
+    return [storedValue, setValue];
 }
